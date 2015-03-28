3f22ad5d2753e024bdf976be2bcff5831ab9e4c2
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2015-03-28 16:41:28 +0000

    commit AC codes for some UVa Online Judge problems

---
+++ trunk/UVa/UVa400.java
@@ -0,0 +1,51 @@
+/* Filename: UVa400.java
+ * Author: Mushiyo
+ */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class UVa400 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		while (input.hasNext()) {
+			int N = input.nextInt();
+			//input.nextLine(); // eat new line
+			String[] filenames = new String[N];
+			int L = 0;
+
+			for (int i = 0; i < filenames.length; ++i) {
+				filenames[i] = input.next();
+				L = Math.max(L, filenames[i].length());
+			}
+
+			Arrays.sort(filenames);
+
+			int C = (60 - L) / (L + 2) + 1;
+			int R = (int) Math.ceil((double) N / C);
+			// System.out.println(C + " " + R + " " + L);
+
+			System.out
+					.println("------------------------------------------------------------");
+
+			for (int r = 0; r < R; ++r) {
+				for (int c = 0; c < C; ++c) {
+					int index = c * R + r;
+
+					if (index < N) {
+						if (index + R < N) {
+							System.out.printf("%-" + (L + 2) + "s",
+									filenames[index]);
+						} else {
+							System.out.printf("%-" + L + "s", filenames[index]);
+						}
+					}
+				}
+				System.out.println();
+			}
+		}
+	}
+
+}
