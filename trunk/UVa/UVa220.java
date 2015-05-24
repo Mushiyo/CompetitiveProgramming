0660903e57ac3b7086f761ec2330ac6acd63ae8f
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2015-05-24 07:28:32 +0000

    update

---
+++ trunk/UVa/UVa220.java
@@ -0,0 +1,20 @@
+/* Filename: UVa220.java
+ * Author: Mushiyo
+ */
+
+import java.util.Scanner;
+
+public class UVa220 {
+	final static char B = 'B';
+	final static char W = 'W';
+	final static char EMPTY = '-';
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		while (input.hasNext()) {
+			
+		}
+	}
+
+}
