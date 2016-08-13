7d0119aceaf81ab045e682afbe14d28d78630741
Mushiyo <Mushiyo@gmail.com>
2016-08-13 23:27:39 +0800

    commit AC codes for some AtCoder problems

---
+++ AtCoder/ARC059C.java
@@ -0,0 +1,33 @@
+/* Filename: ARC059C.java
+ * Author: Mushiyo
+ */
+
+import java.util.Scanner;
+
+public class ARC059C {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		while (input.hasNext()) {
+			int N = input.nextInt();
+			int[] a = new int[N];
+			
+			int sumA = 0;
+			for(int i = 0; i < a.length; ++i){
+				a[i] = input.nextInt();
+				sumA += a[i];
+			}
+			
+			int avgA = (int)Math.round(sumA / (double)N);
+			int cost = 0;
+			
+			for(int i = 0; i < a.length; ++i){
+				cost += (avgA - a[i]) * (avgA - a[i]);
+			}
+			
+			System.out.println(cost);
+		}
+	}
+
+}
