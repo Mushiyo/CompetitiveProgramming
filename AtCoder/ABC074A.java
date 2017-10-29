4893272772bda80eb9008059c47f8b6db554ab61
Mushiyo <Mushiyo@gmail.com>
2017-10-29 22:47:18 +0800

    AC

---
+++ AtCoder/ABC074A.java
@@ -0,0 +1,21 @@
+/* Filename: ABC074A.java
+ * Author: Mushiyo
+ */
+
+import java.util.Scanner;
+
+public class ABC074A {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNext()){
+			int N = input.nextInt();
+			int A = input.nextInt();
+			
+			System.out.println(N * N - A);
+		}
+
+	}
+
+}
